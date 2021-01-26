66042e702ac74d22c42eba9b2b3e423a88ed796e
Marnie Robinson <marnielane@gmail.com>
2021-01-26 19:20:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -214,8 +214,8 @@ def big_shoe_rebounds
   biggest_shoe = 0 
   rebounds = 0 
   game_hash.each do |location, team_details|
-    binding.pry
     team_details.each do |players, player_stats|
+      binding.pry
       if players = :players
         player_stats.each do |player|
           if player[:shoe] > biggest_shoe
